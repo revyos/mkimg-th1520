9e4beba08eb28cab906fa096f1e487cb8e5e0f26
Lu Hui <luhui@sipeed.com>
2023-09-11 15:45:53 +0800

    firstboot: use mac address gen hostname

--- addons/opt/firstboot.sh
+++ addons/opt/firstboot.sh
@@ -6,5 +6,18 @@ parted -s /dev/mmcblk0 "resizepart 3 -0"
 resize2fs /dev/mmcblk0p2
 resize2fs /dev/mmcblk0p3
 
+# hostname with mac address
+HOSTNAME="lpi4a-$(cat /sys/class/net/end0/address | tr -d ':\n' | tail -c 4)"
+for file in /etc/hostname /etc/hosts
+do
+        sed -i -e "s/lpi4a/$HOSTNAME/g" $file
+done
+nmcli general hostname "$HOSTNAME"
+hostname "$HOSTNAME"
+
+# enable & start avahi daemon
+systemctl enable avahi-daemon
+systemctl restart avahi-daemon
+
 # regenerate openssh host keys
 dpkg-reconfigure openssh-server
